43c48ad018d2f82ae0bfa33bb9a5f71914f88a26
stomy13 <48572568+stomy13@users.noreply.github.com>
2025-01-11 00:58:07 +0900

    immer を入れるとエラーが発生する

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { v7 } from "uuid";
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 type SimpleMap = Map<string, string>;
 
@@ -11,7 +12,10 @@ type Action = {
   simpleMapSet: (newSimpleMap: SimpleMap) => void;
 };
 
-export const useStore = create<State & Action>()((set) => ({
-  simpleMap: new Map<string, string>([["1", v7()]]),
-  simpleMapSet: (newSimpleMap: SimpleMap) => set({ simpleMap: newSimpleMap }),
-}));
+export const useStore = create<State & Action>()(
+  immer((set) => ({
+    simpleMap: new Map<string, string>([["1", v7()]]),
+    simpleMapSet: (newSimpleMap: SimpleMap) =>
+      set(() => ({ simpleMap: newSimpleMap })),
+  }))
+);
